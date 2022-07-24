254c88d70baca42d4e7f6953966aadb2bdd4cfe5
amandabledsoe <amandajbledsoe@gmail.com>
2022-07-24 02:02:38 -0400

    This commit minimally covers what we did in class and most of what is required for the Lab; However, I'd like to come back and mess with it some more

---
+++ U7L4/U7L4/Controllers/PaupersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using U7L4.Models;
+
+namespace U7L4.Controllers
+{
+    public class PaupersController : Controller
+    {
+        private readonly ILogger<PaupersController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PaupersController(ILogger<PaupersController> logger, IHttpClientFactory newHttpClientFactory)
+        {
+            _httpClientFactory = newHttpClientFactory;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //1. First, create an HTTP Client via the Client Factory to enable the ability send and receive a response to the API:
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+
+            //2. Second, find the URL to call the API, and set it as a constant for use later.
+            const string redditApiUrl = "https://www.reddit.com/r/aww/.json";
+
+            //3. See if you have to send the API anything. Nothing required in this case.
+
+            //4. Create a Model that models the returned response from the API. 
+            //See 'RedditSimpleResponse.cs'
+
+            //5. Call the API using the GetFromJasonAsync method attached to the HTTP Client. 
+            //Dont forget to add GetAwaiter() and GetResult()
+            var apiResponse = httpClient.GetFromJsonAsync<RedditSimpleResponse>(redditApiUrl).GetAwaiter().GetResult();
+
+            //6. Create a new instance of the API Response Model and store the API response you get into that instance.
+            var redditPostsToDisplay = apiResponse;
+
+            //Finally, pass the API Response Model instance to the view so we can see what what are working with!
+            return View(redditPostsToDisplay);
+        }
+    }
+}
---
+++ U7L4/U7L4/Models/RedditSimpleResponse.cs
@@ -0,0 +1,33 @@
+﻿namespace U7L4.Models
+{
+    public class RedditSimpleResponse
+    {
+        public string kind { get; set; }
+        public RedditSimpleResponse_Data data { get; set; }
+    }
+
+    public class RedditSimpleResponse_Data
+    {
+        public string after { get; set; }
+        public RedditSimpleResponse_Data_Child[] children { get; set; }
+    }
+
+    public class RedditSimpleResponse_Data_Child
+    {
+        public string kind { get; set; }
+        public RedditSimpleResponse_Data_Child_Data data { get; set; }
+    }
+
+    public class RedditSimpleResponse_Data_Child_Data
+    {
+        public string title { get; set; }
+        public RedditSimpleResponse_Data_Child_Data_LinkFlairRichText[] link_flair_richtext { get; set; }
+    }
+
+    public class RedditSimpleResponse_Data_Child_Data_LinkFlairRichText
+    {
+        public string a { get; set; }
+        public string e { get; set; }
+        public string u { get; set; }
+    }
+}
